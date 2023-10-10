90899e412cf2f819401d60ac67480c8f0465249d
ana-zn <anazambrana869@gmail.com>
2023-10-09 23:26:07 -0300

    app3 Semana03

---
+++ Node/01-Hola-Mundo/app3.js
@@ -0,0 +1,12 @@
+console.log('Inicio del programa'); // 1
+setTimeout(()=>{
+    console.log('Primer Timeout'); // 5
+},3000); 
+setTimeout(()=>{
+    console.log('Segundo Timeout'); // 2
+},0); 
+setTimeout(()=>{
+    console.log('Tercer Timeout'); // 3
+},0); 
+
+console.log ('Fin del programa'); // 4
